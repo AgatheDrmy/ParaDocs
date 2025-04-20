18ca15d6e46d4f9245ee08bd864ac5ad2e89857b
Agathe <agathemaud.dormoy@gmail.com>
2025-04-20 16:22:55 +0200

    fix : nav eval

--- src/Controller/EvalController.php
+++ src/Controller/EvalController.php
@@ -6,12 +6,37 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class EvalController extends AbstractController
+#[Route('/eval', name: 'app_eval_')]
+class EvalController extends AbstractController
 {
-    #[Route('/eval', name: 'app_eval')]
-    public function index(): Response
+    #[Route('/', name: 'list')]
+    public function list(): Response
     {
         return $this->render('eval/list_eval.html.twig', [
+            'controller_name' => 'EvalController',
+        ]);
+    }
+
+    #[Route('/comp', name: 'gen')]
+    public function gen(): Response
+    {
+        return $this->render('eval/comp_gen_eval.html.twig', [
+            'controller_name' => 'EvalController',
+        ]);
+    }
+
+    #[Route('/choix', name: 'choix')]
+    public function choix(): Response
+    {
+        return $this->render('eval/choix_question_eval.html.twig', [
+            'controller_name' => 'EvalController',
+        ]);
+    }
+
+    #[Route('/finalisation', name: 'finalisation')]
+    public function finalisation(): Response
+    {
+        return $this->render('eval/finalisation_eval.html.twig', [
             'controller_name' => 'EvalController',
         ]);
     }
