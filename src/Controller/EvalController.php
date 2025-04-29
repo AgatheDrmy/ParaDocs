85f3fd9a360d6ac098bb4afb4ff6368a24d07a60
Agathe <agathemaud.dormoy@gmail.com>
2025-04-29 02:34:40 +0200

    test

--- src/Controller/EvalController.php
+++ src/Controller/EvalController.php
@@ -2,6 +2,22 @@
 
 namespace App\Controller;
 
+<<<<<<< Updated upstream
+=======
+use App\Entity\Evaluation;
+use App\Repository\TypeRepository;
+use App\Repository\DifficulteRepository;
+use App\Repository\ChapitreRepository;
+use App\Repository\ThemeRepository;
+use App\Repository\UtilisateurRepository;
+use App\Repository\QuestionRepository;
+use App\Repository\EvaluationRepository;
+
+use App\Form\EvalFormType;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+>>>>>>> Stashed changes
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -17,27 +33,90 @@ class EvalController extends AbstractController
         ]);
     }
 
-    #[Route('/comp', name: 'gen')]
-    public function gen(): Response
+    #[Route('/{id}/modifier', name: 'modifier')]
+    public function modifier(Request $request,  EntityManagerInterface $em, 
+        Evaluation $evaluation, 
+        TypeRepository $typerepository, 
+        DifficulteRepository $difficulterepository, 
+        ChapitreRepository $chapitrerepository, 
+        ThemeRepository $themerepository,
+        QuestionRepository $questionrepository
+    ): Response
     {
-        return $this->render('eval/comp_gen_eval.html.twig', [
-            'controller_name' => 'EvalController',
+        // $enonce = $question->getEnonce();
+
+        $evalForm = $this->createForm(EvalFormType::class, $evaluation);
+
+        $evalForm->handleRequest($request);
+
+        if($evalForm->isSubmitted() && $evalForm->isValid()){
+            $em->persist($evaluation);
+            $em->flush();
+
+            $this -> addFlash('success', 'question créée');
+
+            return $this->redirectToRoute('apphome');
+        }
+
+        return $this->render('eval/evaluation.html.twig', [
+            'evaluation' => $evaluation,
+            'evalForm' => $evalForm,
+            'type' => $typerepository->findBy([], ['id' => 'asc']),
+            'difficulte' => $difficulterepository->findBy([], ['id' => 'asc']),
+            'chapitre' => $chapitrerepository->findBy([], ['id' => 'asc']),
+            'theme' => $themerepository->findBy([], ['id' => 'asc']),
+            'question' => $questionrepository->findBy([], ['id' => 'asc'])
         ]);
     }
 
+<<<<<<< Updated upstream
     #[Route('/choix', name: 'choix')]
     public function choix(): Response
     {
         return $this->render('eval/choix_question_eval.html.twig', [
             'controller_name' => 'EvalController',
         ]);
+=======
+
+    #[Route('/ajouter', name: 'ajouter')]
+    public function Addeval(Request $request, EntityManagerInterface $em, 
+        TypeRepository $typerepository,
+        ChapitreRepository $chapitrerepository,
+        DifficulteRepository $difficulterepository,
+        UtilisateurRepository $utilisateurrepository,
+        ThemeRepository $themerepository,
+        QuestionRepository $questionrepository
+        ): Response
+    {
+
+        $evaluations = new Evaluation();
+
+        $evalForm = $this->createForm(EvalFormType::class, $evaluations);
+
+        $evaluations->setEvaluationUtilisateur($utilisateurrepository->find(1));
+        $evaluations->setIsActive(1);
+
+        $evalForm->handleRequest($request);
+
+        if($evalForm->isSubmitted() && $evalForm->isValid()){
+            $em->persist($evaluations);
+            $em->flush();
+
+            $this -> addFlash('success', 'question créée');
+
+            return $this->redirectToRoute('apphome');
+        }
+        
+        return $this->render('eval/evaluation.html.twig', [
+            'evalForm' => $evalForm->createView(),
+            'type' => $typerepository->findBy([], ['id' => 'asc']),
+            'chapitre' => $chapitrerepository->findBy([], ['id' => 'asc']),
+            'difficulte' => $difficulterepository->findBy([], ['id' => 'asc']),
+            'theme' => $themerepository->findBy([], ['id' => 'asc']),
+            'question' => $questionrepository->findBy([], ['id' => 'asc'])
+        ]);
+
+>>>>>>> Stashed changes
     }
 
-    #[Route('/finalisation', name: 'finalisation')]
-    public function finalisation(): Response
-    {
-        return $this->render('eval/finalisation_eval.html.twig', [
-            'controller_name' => 'EvalController',
-        ]);
-    }
 }
---
+++ src/Form/EvalFormType.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\difficulte;
+use App\Entity\Evaluation;
+use App\Entity\question;
+use App\Entity\theme;
+use App\Entity\utilisateur;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EvalFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre')
+            ->add('is_calculatrice')
+            ->add('is_ordinateur')
+            ->add('is_document')
+            ->add('autre_modalite')
+            ->add('introduction')
+            ->add('date_heure', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('date_creation', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('duree', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('is_active')
+            ->add('theme_eval', EntityType::class, [
+                'class' => theme::class,
+                'choice_label' => 'id',
+            ])
+            ->add('difficulte_eval', EntityType::class, [
+                'class' => difficulte::class,
+                'choice_label' => 'id',
+            ])
+            ->add('evaluation_utilisateur', EntityType::class, [
+                'class' => utilisateur::class,
+                'choice_label' => 'id',
+            ])
+            ->add('question_evaluation', EntityType::class, [
+                'class' => question::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Evaluation::class,
+        ]);
+    }
+}
